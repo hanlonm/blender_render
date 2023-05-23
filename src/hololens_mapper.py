ba86a5d0c1fc813d9bc09dab45af4b32a9d1a95a
Matthew Hanlon <hanlonm@ethz.ch>
2023-05-23 18:27:39 +0200

    hololens mapper depth mapper

--- src/hololens_mapper.py
+++ src/hololens_mapper.py
@@ -7,6 +7,7 @@ import shutil
 import pytransform3d.rotations as pr
 import pytransform3d.transformations as pt
 from PIL import Image
+import h5py
 
 def main():
     parser = argparse.ArgumentParser()
@@ -19,8 +20,10 @@ def main():
     environment_dataset_path = home_dir / "mt-matthew/data"
     hloc_datasets_path = home_dir / "Hierarchical-Localization/datasets"
     image_output_dir = hloc_datasets_path / environment / "localization"
+    image_output_dir = Path("output")
 
     output_directory = hloc_datasets_path / environment
+    output_directory = Path("output")
 
     pose_file_name = Path(output_directory) / "image_poses.txt"
     pose_file = open(pose_file_name, "w")
@@ -29,9 +32,26 @@ def main():
     hololens_transforms = np.load("/local/home/hanlonm/mt-matthew/hololens_transforms.npy")
     cam_rotation = np.array([[0.0, -0.0, -1.0], [-1.0, 0.0, -0.0],
                              [0.0, 1.0, 0.0]])
+    bproc.init()
+    scene = bproc.loader.load_blend(
+        str(environment_dataset_path) + f"/{environment}/{environment}.blend")
+    bproc.lighting.light_surface(scene, 1.0)
+    # K = [366.8789978027344, 0.0, 240.0, 0.0, 365.71429443359375, 320.0, 0.0, 0.0, 1.0]
+    camera_matrix = np.array([[366.8789978027344, 0, 240],
+                              [0.0, 365.71429443359375, 320], [0, 0.0, 1.0]])
+    bproc.camera.set_intrinsics_from_K_matrix(K=camera_matrix,
+                                              image_width=480,
+                                              image_height=640)
+    bproc.renderer.set_max_amount_of_samples(5)
+    bproc.renderer.set_output_format("JPEG")
+    bproc.renderer.set_denoiser("OPTIX")
+    bproc.renderer.enable_depth_output(activate_antialiasing=True)
+
+    depth_h5 = h5py.File(f"output/{environment}_depth.h5", "w")
+
 
     image_names = []
-    with open("trajectory.txt", "r") as f:
+    with open(str(environment_dataset_path) + f"/{environment}/mapping_trajectory.txt", "r") as f:
         frame = 0
         for line in f.readlines():
             line = [float(x) for x in line.split()]
@@ -44,6 +64,7 @@ def main():
                 cam_pose = pt.pq_from_transform(T_world_base)
 
                 image_file_name = cam + "_" + str(frame).zfill(4) + ".jpeg"
+                image_names.append(image_file_name)
 
                 pose_file.write(
                     "mapping/"
@@ -64,5 +85,28 @@ def main():
                     + str(cam_pose[3])
                     + "\n"
                 )
-                frame += 1
-            rot = rot @ cam_rotation
+                
+
+                p_xyz = cam_pose[:3]
+                q_wxyz = cam_pose[3:]
+                rot = pr.matrix_from_quaternion(q_wxyz)
+                rot = rot @ cam_rotation
+                matrix_world = bproc.math.build_transformation_mat(p_xyz, rot)
+                bproc.camera.add_camera_pose(matrix_world)
+            frame += 1
+
+    data = bproc.renderer.render(output_dir="output")
+    #bproc.writer.write_hdf5("output", data)
+
+    # for i, image_array in enumerate(data["colors"]):
+    #     # Convert the NumPy array to PIL Image
+    #     pil_image = Image.fromarray(image_array)
+
+    #     # Save the image to disk using PIL
+    #     pil_image.save(image_output_dir / image_names[i])
+    for i, depth_array in enumerate(data["depth"]):
+        depth_h5.create_dataset(image_names[i], data=depth_array)
+    depth_h5.close()
+
+if __name__ == "__main__":
+    main()
