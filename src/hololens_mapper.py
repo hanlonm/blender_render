130bc1848a269fb590971a2a7ddb1cb22bf28334
Matthew Hanlon <hanlonm@ethz.ch>
2023-09-07 17:02:35 +0200

    hololens mapper

--- src/hololens_mapper.py
+++ src/hololens_mapper.py
@@ -9,6 +9,7 @@ import pytransform3d.transformations as pt
 from PIL import Image
 import h5py
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--environment", type=str, default="00700")
@@ -19,17 +20,20 @@ def main():
     environment = args.environment
     environment_dataset_path = home_dir / "mt-matthew/data"
     hloc_datasets_path = home_dir / "Hierarchical-Localization/datasets"
-    image_output_dir = hloc_datasets_path / environment / "localization"
-    image_output_dir = Path("output")
+    output_directory: Path = hloc_datasets_path / environment
+    output_directory.mkdir(exist_ok=True)
+    # output_directory = Path("output")
 
-    output_directory = hloc_datasets_path / environment
-    output_directory = Path("output")
+    image_output_dir: Path = hloc_datasets_path / environment / "mapping"
+    image_output_dir.mkdir(exist_ok=True)
+    # image_output_dir = Path("output")
 
     pose_file_name = Path(output_directory) / "image_poses.txt"
     pose_file = open(pose_file_name, "w")
 
     # [dev_LF, dev_LL, dev_RF, dev_RR]
-    hololens_transforms = np.load("/local/home/hanlonm/mt-matthew/hololens_transforms.npy")
+    hololens_transforms = np.load(
+        str(home_dir) + "/mt-matthew/data/hololens_transforms.npy")
     cam_rotation = np.array([[0.0, -0.0, -1.0], [-1.0, 0.0, -0.0],
                              [0.0, 1.0, 0.0]])
     bproc.init()
@@ -51,11 +55,15 @@ def main():
 
 
     image_names = []
-    with open(str(environment_dataset_path) + f"/{environment}/mapping_trajectory.txt", "r") as f:
+    with open(
+            str(environment_dataset_path) +
+            f"/{environment}/mapping_trajectory.txt", "r") as f:
         frame = 0
         for line in f.readlines():
             line = [float(x) for x in line.split()]
-            T_world_dev = pt.transform_from(pr.matrix_from_quaternion(pr.quaternion_wxyz_from_xyzw(line[3:])), line[:3])
+            T_world_dev = pt.transform_from(
+                pr.matrix_from_quaternion(
+                    pr.quaternion_wxyz_from_xyzw(line[3:])), line[:3])
             for i, cam in enumerate(["LF", "LL", "RF", "RR"]):
                 T_dev_cam = pt.transform_from_pq(hololens_transforms[i])
 
@@ -66,26 +74,12 @@ def main():
                 image_file_name = cam + "_" + str(frame).zfill(4) + ".jpeg"
                 image_names.append(image_file_name)
 
-                pose_file.write(
-                    "mapping/"
-                    + image_file_name
-                    + " "
-                    + str(cam_pose[0])
-                    + " "
-                    + str(cam_pose[1])
-                    + " "
-                    + str(cam_pose[2])
-                    + " "
-                    + str(cam_pose[4])
-                    + " "
-                    + str(cam_pose[5])
-                    + " "
-                    + str(cam_pose[6])
-                    + " "
-                    + str(cam_pose[3])
-                    + "\n"
-                )
-                
+                pose_file.write("mapping/" + image_file_name + " " +
+                                str(cam_pose[0]) + " " + str(cam_pose[1]) +
+                                " " + str(cam_pose[2]) + " " +
+                                str(cam_pose[4]) + " " + str(cam_pose[5]) +
+                                " " + str(cam_pose[6]) + " " +
+                                str(cam_pose[3]) + "\n")
 
                 p_xyz = cam_pose[:3]
                 q_wxyz = cam_pose[3:]
@@ -107,6 +101,7 @@ def main():
     for i, depth_array in enumerate(data["depth"]):
         depth_h5.create_dataset(image_names[i], data=depth_array)
     depth_h5.close()
+
 
 if __name__ == "__main__":
     main()
